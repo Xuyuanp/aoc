1a694e8049ef9dd3638ed48b5c6aafacacf82962
Xuyuan Pang <xuyuan.pang@tusen.ai>
2024-03-01 11:18:11 +0800

    day3 part2

--- 2023/src/day3/mod.rs
+++ 2023/src/day3/mod.rs
@@ -1,13 +1,20 @@
+use std::{collections::HashMap, marker::PhantomData};
+
 use crate::Solution;
 
+#[derive(Debug, Clone, Copy, Default)]
 struct Part1;
 
+#[derive(Debug, Clone, Copy, Default)]
+struct Part2;
+
 #[derive(Default)]
-struct EnginSchematic {
+struct EngineSchematic<P> {
     matrix: Vec<Vec<char>>,
+    _marker: PhantomData<P>,
 }
 
-impl EnginSchematic {
+impl<P> EngineSchematic<P> {
     fn height(&self) -> usize {
         self.matrix.len()
     }
@@ -16,45 +23,52 @@ impl EnginSchematic {
         self.matrix[0].len()
     }
 
-    fn neighbours(&self, row: usize, range: (usize, usize)) -> Vec<char> {
+    fn accept(&mut self, line: &str) {
+        self.matrix.push(line.chars().collect());
+    }
+
+    fn neighbours(&self, row: usize, range: (usize, usize)) -> Vec<(usize, usize)> {
         let mut result = vec![];
         if row > 0 {
             for i in range.0..range.1 {
-                result.push(self.matrix[row - 1][i]);
+                result.push((row - 1, i));
             }
             if range.0 > 0 {
-                result.push(self.matrix[row - 1][range.0 - 1]);
+                result.push((row - 1, range.0 - 1));
             }
             if range.1 < self.width() {
-                result.push(self.matrix[row - 1][range.1]);
+                result.push((row - 1, range.1));
             }
         }
 
         if range.0 > 0 {
-            result.push(self.matrix[row][range.0 - 1]);
+            result.push((row, range.0 - 1));
         }
         if range.1 < self.width() {
-            result.push(self.matrix[row][range.1]);
+            result.push((row, range.1));
         }
 
         if row < self.height() - 1 {
             for i in range.0..range.1 {
-                result.push(self.matrix[row + 1][i]);
+                result.push((row + 1, i));
             }
             if range.0 > 0 {
-                result.push(self.matrix[row + 1][range.0 - 1]);
+                result.push((row + 1, range.0 - 1));
             }
             if range.1 < self.width() {
-                result.push(self.matrix[row + 1][range.1]);
+                result.push((row + 1, range.1));
             }
         }
         result
     }
+}
 
+impl EngineSchematic<Part1> {
     fn is_valid(&self, row: usize, range: (usize, usize)) -> bool {
         self.neighbours(row, range)
             .iter()
-            .any(|&c| !(c.is_ascii_digit() || c == '.'))
+            .map(|&(i, j)| self.matrix[i][j])
+            .any(|c| !(c.is_ascii_digit() || c == '.'))
     }
 
     fn result(&mut self) -> usize {
@@ -86,14 +100,61 @@ impl EnginSchematic {
         }
         numbers.iter().sum()
     }
+}
 
-    fn accept(&mut self, line: &str) {
-        self.matrix.push(line.chars().collect());
+impl EngineSchematic<Part2> {
+    fn result(&mut self) -> usize {
+        let mut gears: HashMap<(usize, usize), Vec<usize>> = HashMap::new();
+
+        for i in 0..self.height() {
+            let mut start = None;
+            let mut number = 0;
+
+            for j in 0..self.width() {
+                let c = self.matrix[i][j];
+                if let Some(n) = c.to_digit(10) {
+                    if start.is_none() {
+                        start = Some(j);
+                    }
+                    number = number * 10 + (n as usize);
+                } else if let Some(s) = start {
+                    self.neighbours(i, (s, j))
+                        .iter()
+                        .filter(|(x, y)| self.matrix[*x][*y] == '*')
+                        .for_each(|&(x, y)| {
+                            gears.entry((x, y)).or_default().push(number);
+                        });
+
+                    number = 0;
+                    start = None;
+                }
+            }
+            if let Some(s) = start {
+                self.neighbours(i, (s, self.width()))
+                    .iter()
+                    .filter(|(x, y)| self.matrix[*x][*y] == '*')
+                    .for_each(|&(x, y)| {
+                        gears.entry((x, y)).or_default().push(number);
+                    });
+            }
+        }
+        gears
+            .values()
+            .filter(|v| v.len() == 2)
+            .map(|v| v[0] * v[1])
+            .sum()
     }
 }
 
-impl From<EnginSchematic> for usize {
-    fn from(value: EnginSchematic) -> Self {
+impl From<EngineSchematic<Part1>> for usize {
+    fn from(value: EngineSchematic<Part1>) -> Self {
+        let mut value = value;
+        value.result()
+    }
+}
+
+impl From<EngineSchematic<Part2>> for usize {
+    fn from(value: EngineSchematic<Part2>) -> Self {
         let mut value = value;
         value.result()
     }
@@ -102,7 +163,34 @@ impl From<EnginSchematic> for usize {
 impl Solution for Part1 {
     type Item = &'static str;
 
-    type Acc = EnginSchematic;
+    type Acc = EngineSchematic<Self>;
+
+    type Output = usize;
+
+    const EXAMPLE: &'static str = r#"467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."#;
+
+    const INPUT: &'static str = include_str!("input.txt");
+
+    fn calculate(acc: Self::Acc, item: Self::Item) -> Self::Acc {
+        let mut acc = acc;
+        acc.accept(item);
+        acc
+    }
+}
+
+impl Solution for Part2 {
+    type Item = &'static str;
+
+    type Acc = EngineSchematic<Self>;
 
     type Output = usize;
 
@@ -134,6 +222,13 @@ mod tests {
     fn test_part1() {
         let sol = Part1;
         assert_eq!(sol.run_example(), 4361);
-        assert_eq!(sol.run(), 4361);
+        assert_eq!(sol.run(), 535078);
+    }
+
+    #[test]
+    fn test_part2() {
+        let sol = Part2;
+        assert_eq!(sol.run_example(), 467835);
+        assert_eq!(sol.run(), 75312571);
     }
 }
